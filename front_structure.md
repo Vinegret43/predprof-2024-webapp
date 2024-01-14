35e98af3ed7f1a1e84c5697d85b70d76537c9158
Vinegret43 <fvinegret@mail.ru>
2024-01-14 21:11:19 +0300

    Implement base for user.html

--- front_structure.md
+++ front_structure.md
@@ -4,7 +4,5 @@ Front-end structure:
  - - login.html - Когда пользователь не авторизован
  - - user.html - Когда пользователь - обычный юзер
  - - admin.html - Админка
- - /board?id=<ID>
- - - board.html
  - prizes.html
 
--- ui/static/js/user.js
+++ ui/static/js/user.js
@@ -1,1 +1,48 @@
- 
+// Крч надо:
+// 1. Сделать, чтобы скрипт запрашивал данные об игровых полях с сервера, парсил
+// их и затем уже генерировал HTML для игровых полей на основе полученных данных.
+// Можно это делать с помощью createBoard, если чё. Генерацию HTML я прописал уже
+// 2. Сделать, чтобы клетки в таблице отвечали на клики и посылали запрос на
+// сервер, говоря, что игрок хочет совершить выстрел. Я оставил TODO там,
+// где надо добавить коллбэк (Только для клеток, в которые ещё не стреляли,
+// само собой). Эта самая клетка потом ещё должна будет обновить себя
+// взависимости от полученного с сервака результата
+
+function createBoard(title, shots, size, content) {
+    // Taking a pre-made template and just modifying it
+    let element = document.getElementsByClassName("board")[0].cloneNode(true);
+
+    element.getElementsByClassName("board_title")[0].innerHTML = title;
+    element.getElementsByClassName("board_shots")[0].innerHTML = "Количество выстрелов: " + shots;
+
+    let table = element.getElementsByTagName("table")[0];
+
+    for (let y = 0; y < size; y++) {
+        console.log(document.createElement);
+        let row = document.createElement("tr");
+        for (let x = 0; x < size; x++) {
+            let entry = document.createElement("td");
+            if (content[y * size + x] == "unknown") {
+               entry.innerHTML = "[]";
+                // TODO: Attach a callback to `entry` to react to clicks
+            } else if (content[y * size + x] == "empty") {
+               entry.innerHTML = "X";
+            } else {
+                entry.innerHTML = "P";
+            }
+            row.appendChild(entry);
+        }
+        table.appendChild(row);
+    }
+
+    return element;
+}
+
+// This code is just for reference: delete it
+let content = [
+    "unknown", "unknown", "empty",
+    "empty", "unknown", "unknown",
+    "prize.png", "unknown", "unknown",
+];
+document.getElementsByClassName("main")[0].appendChild(createBoard("Board title", 12, 3, content));
+document.getElementsByClassName("main")[0].appendChild(createBoard("Board title 2", 3, 3, content));

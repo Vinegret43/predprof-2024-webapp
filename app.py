bd5b267768b72ea88fe5b0ceaec55ef7123c1a85
Vinegret43 <fvinegret@mail.ru>
2024-01-19 20:55:22 +0300

    Rewrite app.py

--- app.py
+++ app.py
@@ -1,139 +1,164 @@
-from flask import Flask, jsonify, redirect, url_for, session
-
+from flask import Flask, jsonify, redirect, url_for, session, render_template, request
 
 app = Flask(__name__)
+app.secret_key = "S2;lJ^}S8F3[..xf{a}Ju%9%DpSK#iaAXRW;c(J{Neb!lTy^oZoB1tyz!.yF,HD"
 
-@app.route('/api/login?user_name=<str:user_name>&password=<str:password>', methods=['POST'])
-def login(user_name, password):
-    admin = 0 #Получение из ИЗ роли пользователя, если не находит пользоватя по переданным данным, то = None
-    if admin is None:
-        return 'null'
+@app.get('/')
+def main(error_message=None):
+    if 'username' in session:
+        is_admin = 0 # TODO: Запросить из БД
+        if is_admin:
+            return render_template('admin.html')
+        else:
+            return render_template('user.html')
+    return render_template('login.html', error_message=error_message)
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    valid_user = False # TODO: Подтвердить пароль пользователя в БД
+    if valid_user:
+        session['username'] = username
+        return redirect(url_for('main'))
     else:
-        return jsonify({'admin': admin})
+        return main("Неверное имя пользователя или пароль")
 
+@app.post('/api/register')
+def register():
+    username = request.form["username"]
+    password = request.form["password"]
+    is_admin = bool(request.form["is_admin"])
+    
+    if not username.strip() or not password.strip():
+        return main("Недопустимое имя пользователя или пароль")
 
-@app.route('/api/register?user_name=<str:user_name>&password=<str:password>&admin=<bool:admin>', methods=['POST'])
-def register(user_name, password, admin):
-    user_id = 0 #Запрос из БД количество пользователей
+    user_exists = False # TODO: попробовать записать пользователя в БД. Если пользователь
+    # уже существует, записать в эту переменную True
 
-    # Запись в БД нового пользователя
-
-
-@app.route('/api/boards', methods=['GET'])
-def boards():
-    boards = 0 #Запрос из БД списка словарей ([{'id': int, 'size': int, 'name': str}])
-
-    return jsonify({'boards': boards})
-
-
-@app.route('/api/createBoard?board_name=<str:board_name>&size=<int:size>', methods=['POST'])
-def createBoard(board_name, size):
-    id = 0 #Запрос из БД количество полей
-    # Запись в БД нового поля 
-
-
-@app.route('/api/deleteBoard?board_id=<int:board_id>', methods=['POST'])
-def deleteBoard(id):
-    #Запрос в БД удаление поля по id
-    pass
-
-
-@app.route('/api/prizes', methods=['GET'])
-def prizes():
-    prizes = 0 #Запрос из БД списка словарей ([{'name': str, 'image': str, 'description': str, 'id': int, 'status': str}]) (Статусы: prize, not_prize, not_shot)
-
-    return jsonify({'prizes': prizes})
-
-
-@app.route('/api/createPrize?prize_name=<str:prize_name>&description=<str:description>', methods=['POST']) #TODO Сделать приемку файла
-def createPrize(prize_name, description):
-    id = 0 #Запрос из БД количество призов
-    #Запис в БД нового приза
-
-
-@app.route('/api/editPrize?id=<int:id>&prize_name=<str:prize_name>&description=<str:description>', methods=['POST']) #TODO Сделать приемку файла 
-def editPrize(id, prize_name=None, description=None):
-    if prize_name is None:
-        prize_name = 0 #Запрос name из БД
-    if description is None:
-        description = 0 #Запрос description из БД
-
-    #Запрос в БД обновление данных о призе
-    pass
-
-
-@app.route('/api/deletePrize?prize_id=<int:prize_id>', methods=['POST'])
-def deletePrize(prize_id):
-    #Запрос в БД удаление приза по id
-    pass
-
-
-@app.route('/api/putPrize?board_id=<int:board_id>&prize_id=<int:prize_id>&x=<int:x>&y=<int:y>', methods=['POST'])
-def putPrize(board_id, prize_id, x, y):
-    #Запись в БД в таблицу полей по (board_id) prize_id, x, y
-    pass
-
-
-@app.route('/api/users', methods=['GET']) 
-def users():
-    users = 0 #Запрос из БД списка словарей ([{'user_name': str, 'user_id': int}])
-
-    return jsonify({'users': users})
-
-
-@app.route('/api/addPlayer?board_id=<int:board_id>&user_id=<int:user_id>', methods=['POST'])
-def addPlayer(board_id, user_id):
-    #Запись в БД в таблицу полей по (board_id) user_id в user_id_list
-    pass
-
-
-@app.route('/api/setNumberOfShots?user_id=<int:user_id>&board_id=<int:board_id>&shots=<int:shots>', methods=['POST'])
-def setNumberOfShots(user_id, board_id, shots):
-    #Запись в БД в таблицу полей по (board_id) {user_id:shots} в user_shots_dict
-    pass
-
-
-@app.route('/api/boardsforuser?user_id=<int:user_id>', methods=['GET']) #TODO В ТЗ другое, надо обсудить
-def boardsforuser(user_id):
-    boards = [] #Запрос из БД списка словарей ([{'id': int, 'size': int, 'name': str, 'user_id_list': list}])
-    for board in boards:
-        if user_id not in board['user_id_list']:
-            boards.pop(board)
-
-    return jsonify({'boards': boards})
-
-
-@app.route('/api/board?board_id=<int:board_id>', methods=['GET']) 
-def board(board_id):
-    board_info = [] #Запрос из БД списка словарей ([{'id': int, 'size': str, 'name': str, 'user_id_list': list, 'fields_list': list}])
-
-    return jsonify({'board_info': board_info})
-
-
-@app.route('/api/shoot?board_id=<int:board_id>&x=<int:x>&y=<int:y>', methods=['POST']) 
-def shoot(board_id, x, y):
-    field_info = [] #Запрос из БД списка информации о поле ({'name': str, 'image': str, 'description': str, 'status': str}), если ее нет то None, (Статусы: prize, not_prize, not_shot)
-    if field_info is None:
-        return 'null'
+    if user_exists:
+        return main("Имя пользователя занято")
     else:
-        return jsonify({'field_info': field_info})
+        session["username"] = username
+        return redirect(url_for("main"))
 
-
-@app.route('/api/prizes', methods=['GET']) 
-def prizes():
-    return 'prizes.html'
-
-
-@app.route('/api/logout', methods=['POST']) 
+@app.get('/api/logout') 
 def logout(): 
-    session['logged_in'] = False
+    session.clear()
     return redirect(url_for('main'))
 
+@app.get('/api/boards')
+def boards():
+    username = session["username"]
+    is_admin = False # TODO: Запросить у бд, является ли пользователь админом
+    if is_admin:
+        boards = [] # TODO: Запросить все существующие таблицы
+    else:
+        boards = [] # TODO: Запросить все таблицы, доступные данному юзеру
+    return jsonify(boards)
 
-@app.route('/api/logout', methods=['POST']) 
-def logout():
-    return redirect(url_for('main'))
+@app.post('/api/createBoard')
+def create_board():
+    name = request.form["name"]
+    size = int(request.form["size"])
+    assert size > 2
+    board_id = 1337 # TODO: Создать новую таблицу в БД, вернуть её ID
+    return str(board_id)
 
+@app.post('/api/deleteBoard')
+def delete_board(id):
+    board_id = int(request.form("board_id"))
+    # TODO: Запрос в БД удаление поля по id
+
+@app.get('/api/prizes')
+def prizes():
+    is_admin = False # TODO: Запросить у БД, является ли пользователь админом 
+    if is_admin:
+        prizes = [] # TODO: Запрос из БД списка словарей формата {'name': str, 'image': str, 'description': str, 'id': int, 'isWon': bool}
+        return jsonify(prizes)
+    else:
+        return render_template("prizes.html")
+
+@app.post('/api/createPrize')
+def create_prize():
+    name = request.form["name"]
+    description = request.form["description"]
+    # TODO: принимать файл с иконкой
+    id = 1337 # TODO: создать приз в БД, вернуть ID
+    return str(id)
+
+@app.post('/api/editPrize')
+def edit_prize():
+    id = int(request.form["id"])
+    if "name" in request.form:
+        new_name = request.form["name"]
+        # TODO: Обновить имя в БД
+    if "description" in request.form:
+        new_description = request.form["description"]
+        # TODO: Обновить описание в БД
+    # TODO: обновление иконки
+
+@app.post('/api/deletePrize')
+def delete_prize():
+    id = int(request.form["id"])
+    # TODO: Запрос в БД на удаление приза по id
+
+@app.post('/api/putPrize')
+def put_prize(board_id, prize_id, x, y):
+    board_id = int(request.form("board_id"))
+    prize_id = int(request.form("prize_id"))
+    x = int(request.form("x"))
+    y = int(request.form("y"))
+    # TODO: Добавить приз в клетку поля
+
+@app.post('/api/clearPrize')
+def clear_prize(board_id, prize_id, x, y):
+    board_id = int(request.form("board_id"))
+    x = int(request.form("x"))
+    y = int(request.form("y"))
+    # TODO: Удалить приз из клетки поля
+
+@app.get('/api/users') 
+def users():
+    users = [] # TODO: Запрос из БД списка словарей формата {'username': str, 'user_id': int}
+    return jsonify(users)
+
+@app.post('/api/addPlayer')
+def add_player(board_id, user_id):
+    board_id = int(request.form("board_id"))
+    user_id = int(request.form("user_id"))
+    # TODO: Дать игроку доступ к полю в БД
+
+@app.post('/api/setNumberOfShots')
+def set_number_of_shots(user_id, board_id, shots):
+    user_id = int(request.form["user_id"])
+    board_id = int(request.form["board_id"])
+    shots = int(request.form["shots"])
+    # TODO: Запись в БД в таблицу полей по (board_id) {user_id:shots} в user_shots_dict
+
+@app.get('/api/board') 
+def board(board_id):
+    id = int(request.form["id"])
+    board_content = [] # TODO: Возвратить информацию о каждой клетке поля в формате списка.
+    # Значения идут построчно: сначала значения первой строки слева направо, затем второй и
+    # и так далее. Информация о каждой клетке закодирована в формате строки. Возможны
+    # следующие значения:
+    # - "unknown" - Никто ещё не стрелял в эту клетку. Неизвестно, что там
+    # - "empty" - В клетку уже стреляли и она оказалась пустой
+    # - "/path/to/icon.png" - Здесь есть приз, его уже выиграли. Строка содержит путь
+    #  до иконки приза
+
+    return jsonify(board_content)
+
+@app.post('/api/shoot') 
+def shoot(board_id, x, y):
+    board_id = int(request.form["board_id"])
+    x = int(request.form["x"])
+    y = int(request.form["y"])
+    field_info = None # TODO: Запросить из БД информацию о данной клетке.
+    # Возвращает либо None, либо словарь с информацией о выигранном призе следующего
+    # формата: {"name": str, "image": str, "description": str}
+    return jsonify(field_info)
 
 if __name__ == '__main__':
     app.run()
